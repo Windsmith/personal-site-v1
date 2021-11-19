b4cf7313c5662ce6b1477234be9a6525ec2432af
Windsmith <shriramsekar11@gmail.com>
2021-11-19 10:12:08 +0400

    feat: make the site a little more responsive.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,25 @@
 <div class="hero my-auto z-10">
 	<div class="hero-content text-center">
 		<div>
-			<h1 class="font-bold text-5xl mt-5 mx-auto font-worksans mx-10">Hi There!</h1>
-			<h2 class="font-bold text-5xl mb-5 font-worksans mx-2">I am Shriram</h2>
-			<p class="font-semibold text-xl font-worksans mx-5">
+			<h1 class="font-bold text-5xl mt-5 mx-auto font-worksans mx-10 xl:text-7xl xl:mt-20">
+				Hi There!
+			</h1>
+			<h2 class="font-bold text-5xl mb-5 font-worksans mx-2 xl:text-7xl">I am Shriram</h2>
+			<p class="font-semibold text-xl font-worksans mx-5 md:mx-16 md:mt-14 xl:text-3xl xl:mx-44">
 				I am a frontend developer and am currently exploring backend and devops at my own pace. I'm
 				very curious and hold an interest in various topics!
 			</p>
 			<p class="text-2xl my-5 mt-12 font-semibold font-worksans">Do check my work out :)</p>
 			<div>
 				<a
-					class="btn font-worksans mr-4 bg-gray-900"
+					class="btn font-worksans mr-4 bg-gray-900 xl:text-xl"
 					href="https://hashnode.com/@windsmith"
 					target="_blank"
 					rel="noopener noreferrer"
 					>My Blog
 				</a>
 				<a
-					class="btn font-worksans bg-gray-900"
+					class="btn font-worksans bg-gray-900 xl:text-xl"
 					href="https://github.com/Windsmith"
 					target="_blank"
 					rel="noopener noreferrer"
