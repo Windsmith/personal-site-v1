1d4f7fea9c5d99bb443f7c71c6ed7f48e24124be
Windsmith <shriramsekar11@gmail.com>
2021-11-19 10:42:20 +0400

    feat: set up deploy settings for ghpages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'main',
+        repo: 'https://github.com/Windsmith/personal-site.git', // Update to point to your repository
+        user: {
+            name: 'Shriram', // update to use your name
+            email: 'shriramsekar11@gmail.com' // Update to use your email
+        },
+        dotfiles: true
+    },
+    () => {
+        console.log('Deploy Complete!');
+    }
+);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,8 +6,9 @@
 			</h1>
 			<h2 class="font-bold text-5xl mb-5 font-worksans mx-2 xl:text-7xl">I am Shriram</h2>
 			<p class="font-semibold text-xl font-worksans mx-5 md:mx-16 md:mt-14 xl:text-3xl xl:mx-44">
-				I am a frontend developer and am currently exploring backend and devops at my own pace. I'm
-				very curious and hold an interest in various topics!
+				I am a frontend developer and am currently exploring backend and devops at my own pace to
+				learn how to build a complete application. I'm very curious and hold an interest in various
+				topics!
 			</p>
 			<p class="text-2xl my-5 mt-12 font-semibold font-worksans">Do check my work out :)</p>
 			<div>
