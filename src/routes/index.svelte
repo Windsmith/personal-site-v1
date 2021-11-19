91da48cff1a137bb79e85c663e4d59a1bcee4cb0
Windsmith <shriramsekar11@gmail.com>
2021-11-19 17:01:02 +0400

    feat: make the site fully responsive (I think it is)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,39 @@
-<div class="hero z-10 mb-20">
+<script>
+	import { fly } from 'svelte/transition';
+</script>
+
+<div class="hero z-10 mb-20 xl:mb-10">
 	<div class="hero-content text-center">
 		<div>
-			<h1 class="font-bold text-gray-900 text-5xl mx-auto font-worksans mx-10 xl:text-7xl">
+			<h1 class="font-bold text-gray-900 text-5xl mx-auto font-worksans mx-10 md:text-7xl" in:fly>
 				Hi There!
 			</h1>
-			<h2 class="font-bold text-gray-900 text-5xl mb-5 font-worksans mx-2 xl:text-7xl">
+			<h2
+				in:fly={{ y: -20, duration: 500 }}
+				class="font-bold text-gray-900 text-5xl mb-5 font-worksans mx-2 md:mb-16 md:text-7xl"
+			>
 				I am Shriram
 			</h2>
 			<p
-				class="font-semibold text-xl text-gray-900 font-worksans mx-5 md:mx-16 md:mt-14 xl:text-3xl xl:mx-44"
+				class="font-semibold text-xl text-gray-900 font-worksans mx-5 mt-10 md:text-3xl md:mx-16 md:mt-14 xl:mx-44"
 			>
 				I am a frontend developer and am currently exploring backend and devops at my own pace to
 				learn how to build a complete application. I'm very curious and hold an interest in various
 				topics!
 			</p>
-			<p class="text-2xl my-5 mt-12 text-gray-900 font-semibold font-worksans">
+			<p class="text-2xl my-5 mt-8 text-gray-900 font-semibold font-worksans md:mt-12 md:text-4xl">
 				Do check my work out :)
 			</p>
 			<div>
 				<a
-					class="btn font-worksans mr-4 bg-gray-900 xl:text-xl"
+					class="btn font-worksans mr-8 bg-gray-900 text-base md:text-2xl"
 					href="https://hashnode.com/@windsmith"
 					target="_blank"
 					rel="noopener noreferrer"
 					>Blog
 				</a>
 				<a
-					class="btn font-worksans bg-gray-900 xl:text-xl"
+					class="btn font-worksans bg-gray-900 text-base md:text-2xl"
 					href="https://github.com/Windsmith"
 					target="_blank"
 					rel="noopener noreferrer"
